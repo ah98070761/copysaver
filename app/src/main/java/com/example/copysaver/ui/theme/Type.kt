a1a9d4e826223f7b3624793bd0df204505304727
ah98070761 <ah98070761@gmail.com>
2025-10-15 09:28:57 +0300

    Create app/src/main/java/com/example/copysaver/ui/theme/Type.kt

---
+++ app/src/main/java/com/example/copysaver/ui/theme/Type.kt
@@ -0,0 +1,8 @@
+package com.example.copysaver.ui.theme
+
+import androidx.compose.material3.Typography
+
+// تعريف نمط الطباعة (Typography)
+val Typography = Typography(
+    // يمكنك إضافة أنماط محددة هنا
+)
