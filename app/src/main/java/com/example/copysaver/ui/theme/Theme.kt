6e0747edbdc49499cc689d955c831cbd94e332e5
ah98070761 <ah98070761@gmail.com>
2025-10-15 09:28:31 +0300

    Create app/src/main/java/com/example/copysaver/ui/theme/Theme.kt

---
+++ app/src/main/java/com/example/copysaver/ui/theme/Theme.kt
@@ -0,0 +1,39 @@
+package com.example.copysaver.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80,
+    background = Color.Black
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40,
+    background = Color.White
+)
+
+@Composable
+fun CopySaverTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography, 
+        content = content
+    )
+}
